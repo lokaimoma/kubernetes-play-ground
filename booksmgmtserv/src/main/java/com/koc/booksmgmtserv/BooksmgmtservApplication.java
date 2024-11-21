ee98c5a34b27a1d52f91a2dedcdf39442fec48e5
Owusu Kelvin Clark <kelvinclark@lokaitech.com>
2024-11-21 11:16:42 +0100

    Add book management service

---
+++ booksmgmtserv/src/main/java/com/koc/booksmgmtserv/BooksmgmtservApplication.java
@@ -0,0 +1,13 @@
+package com.koc.booksmgmtserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksmgmtservApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksmgmtservApplication.class, args);
+	}
+
+}
---
+++ booksmgmtserv/src/test/java/com/koc/booksmgmtserv/BooksmgmtservApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koc.booksmgmtserv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksmgmtservApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
