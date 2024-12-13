df7f077203b5042593aed1c8f6c30be9296ef742
Owusu Kelvin Clark <kelvinclark@lokaitech.com>
2024-12-13 10:56:32 +0100

    Add oauth2server config to filter chain

--- gateway/src/main/java/com/koc/gateway/SecurityConfig.java
+++ gateway/src/main/java/com/koc/gateway/SecurityConfig.java
@@ -4,7 +4,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.util.matcher.PathPatternParserServerWebExchangeMatcher;
+import reactor.core.publisher.Flux;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 
 @Configuration
@@ -14,7 +26,28 @@ public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
         http.csrf(ServerHttpSecurity.CsrfSpec::disable);
-        http.authorizeExchange(exchange -> exchange.anyExchange().permitAll());
+        http.authorizeExchange(exchange -> exchange.pathMatchers("/umgmt/auth/signup", "/umgmt/auth/login")
+                        .permitAll()
+                        .anyExchange().authenticated())
+                .oauth2ResourceServer(serverSpec -> {
+                    serverSpec.jwt(jwtSpec -> {
+                        jwtSpec.jwtAuthenticationConverter(jwtAuthenticationConverter());
+                    });
+                });
         return http.build();
+    }
+
+    @Bean
+    public ReactiveJwtAuthenticationConverter jwtAuthenticationConverter() {
+        ReactiveJwtAuthenticationConverter converter = new ReactiveJwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            var realmRoles = Optional.ofNullable((Map<String, Object>) jwt.getClaims().get("realm_roles"));
+            var roles = realmRoles.flatMap(r -> Optional.ofNullable((List<String>) r.get("roles")));
+            var stream = roles.stream().flatMap(Collection::stream)
+                    .map(SimpleGrantedAuthority::new)
+                    .map(GrantedAuthority.class::cast);
+            return Flux.fromStream(stream);
+        });
+        return converter;
     }
 }
